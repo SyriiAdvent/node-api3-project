fd5c3e93306690104866912faefe781ef5c2ba05
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-09 09:52:38 -0700

    deploy test commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const server = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 // Middleware imports
 const helmet = require('helmet');
 const morgan = require('morgan');
