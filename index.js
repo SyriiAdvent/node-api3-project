9f5101e5d412613b6938eda9845fcc11c7cbbb1e
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-08 21:32:46 -0700

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const server = express();
-const PORT = 5000
+const PORT = process.env.PORT
 // Middleware imports
 const helmet = require('helmet');
 const morgan = require('morgan');
